fa4bf3927867e835c7d14d56602f007b39517ad9
Edouard Dupin <yui.heero@gmail.com>
2012-08-15 20:48:28 +0200

    simplify makefiles

--- Generic.mk
+++ Generic.mk
@@ -8,13 +8,19 @@ LOCAL_MODULE := lua
 # name of the dependency
 LOCAL_STATIC_LIBRARIES := etk
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/lua/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/lua/
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_CFLAGS := -DLUA_COMPAT_ALL \
-                -DLUA_USE_LINUX \
-                -DLUA_VERSION_TAG_NAME="\"5.2-$(BUILD_DIRECTORY_MODE)\""
+                -DLUA_VERSION_TAG_NAME="\"5.2-$(BUILD_DIRECTORY_MODE)\"" \
+                -Wall
+
+ifeq ("$(TARGET_OS)","Windows")
+	LOCAL_CFLAGS += -D_WIN32
+else
+	LOCAL_CFLAGS += -DLUA_USE_LINUX
+endif
 
 # load the common sources file of the platform
 include $(LOCAL_PATH)/file.mk
@@ -22,5 +28,3 @@ include $(LOCAL_PATH)/file.mk
 LOCAL_SRC_FILES := $(FILE_LIST)
 
 include $(BUILD_STATIC_LIBRARY)
-
-
